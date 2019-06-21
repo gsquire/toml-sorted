99e85a46a12a5e2c8a4e2dc6c50c81cbc2012a0d
Garrett Squire <garrettsquire@gmail.com>
2019-06-21 15:08:51 -0700

    add a successful case

--- tests/integration.rs
+++ tests/integration.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 use assert_cmd::prelude::*;
 
 #[test]
-fn issue_2() {
+fn issue_2_manifest() {
     let mut cmd = Command::cargo_bin("toml-sorted").unwrap();
     cmd.arg("manifests/issue_2.toml");
     cmd.assert().failure().code(1);
@@ -15,4 +15,11 @@ fn cargo_expand_manifest() {
     cmd.arg("manifests/cargo-expand.toml");
     // TODO: Should this be considered a failure?
     cmd.assert().failure();
+}
+
+#[test]
+fn crossbeam_channel_manifest() {
+    let mut cmd = Command::cargo_bin("toml-sorted").unwrap();
+    cmd.arg("manifests/crossbeam-channel.toml");
+    cmd.assert().success();
 }
