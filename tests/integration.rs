4e22bace874676e7d1353012fe1c112134781680
Garrett Squire <garrettsquire@gmail.com>
2019-06-23 12:53:48 -0700

    fix special case and add workspace test

--- tests/integration.rs
+++ tests/integration.rs
@@ -13,8 +13,7 @@ fn issue_2_manifest() {
 fn cargo_expand_manifest() {
     let mut cmd = Command::cargo_bin("toml-sorted").unwrap();
     cmd.arg("manifests/cargo-expand.toml");
-    // TODO: Should this be considered a failure?
-    cmd.assert().failure();
+    cmd.assert().success();
 }
 
 #[test]
@@ -22,4 +21,11 @@ fn crossbeam_channel_manifest() {
     let mut cmd = Command::cargo_bin("toml-sorted").unwrap();
     cmd.arg("manifests/crossbeam-channel.toml");
     cmd.assert().success();
+}
+
+#[test]
+fn workspace_manifest() {
+    let mut cmd = Command::cargo_bin("toml-sorted").unwrap();
+    cmd.arg("manifests/workspace.toml");
+    cmd.assert().failure().code(1);
 }
