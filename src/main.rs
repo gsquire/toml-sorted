a4cc0bd0318c650c59af881d1eecdf87250029a0
Garrett Squire <github@garrettsquire.com>
2026-03-14 11:59:00 -0700

    feat: major upgrades
    This patch revives this crate after many years.
    
    Refs: #7

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,40 @@
-use std::error::Error;
 use std::path::PathBuf;
-use std::{fmt, fs, io, process};
 
-use structopt::StructOpt;
-use toml::{de, Value};
-
-#[derive(Debug)]
-enum CommandErr {
-    TomlError(de::Error),
-    IoError(io::Error),
-}
-
-impl fmt::Display for CommandErr {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            CommandErr::TomlError(_) => write!(f, "could not parse manifest"),
-            CommandErr::IoError(_) => write!(f, "could not read manifest"),
-        }
-    }
-}
-
-impl Error for CommandErr {}
-
-impl From<de::Error> for CommandErr {
-    fn from(e: de::Error) -> Self {
-        CommandErr::TomlError(e)
-    }
-}
-
-impl From<io::Error> for CommandErr {
-    fn from(e: io::Error) -> Self {
-        CommandErr::IoError(e)
-    }
-}
+use anyhow::Context;
+use toml::{Table, Value};
 
 // Represents a parsed manifest with a field maintaining the file contents.
 struct Manifest {
-    value: Value,
+    table: Table,
     parsed_file: String,
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "toml-sorted", about = "Check if Cargo.toml is sorted.")]
+#[derive(Debug, argh::FromArgs)]
+/// Check if your Cargo manifest is sorted.
 struct Opt {
-    #[structopt(parse(from_os_str))]
+    /// the manifest to check
+    #[argh(positional)]
     manifest: PathBuf,
 }
 
-fn parse_manifest(options: &Opt) -> Result<Manifest, CommandErr> {
-    let manifest = fs::read_to_string(&options.manifest)?;
-    let v = manifest.parse::<Value>()?;
+fn parse_manifest(options: &Opt) -> anyhow::Result<Manifest> {
+    let manifest = std::fs::read_to_string(&options.manifest).with_context(|| {
+        format!(
+            "failed to read manifest at path {}",
+            options.manifest.display()
+        )
+    })?;
+    let table = manifest
+        .parse::<Table>()
+        .context("failed to parse manifest")?;
     Ok(Manifest {
-        value: v,
+        table,
         parsed_file: manifest,
     })
 }
 
 fn check_workspace_by_key(workspace: &Value, key: &str) -> bool {
-    workspace.get(key).map_or(true, |v| {
+    workspace.get(key).is_none_or(|v| {
         let arrays = v.as_array().unwrap();
         let values = arrays
             .iter()
@@ -67,14 +44,14 @@ fn check_workspace_by_key(workspace: &Value, key: &str) -> bool {
     })
 }
 
-fn check_workspace(manifest: &Value) -> bool {
-    manifest.get("workspace").map_or(true, |v| {
-        check_workspace_by_key(&v, "exclude") && check_workspace_by_key(&v, "members")
+fn check_workspace(manifest: &Table) -> bool {
+    manifest.get("workspace").is_none_or(|v| {
+        check_workspace_by_key(v, "exclude") && check_workspace_by_key(v, "members")
     })
 }
 
 fn check_deps_by_key(manifest: &Manifest, key: &str) -> bool {
-    manifest.value.get(key).map_or(true, |v| {
+    manifest.table.get(key).is_none_or(|v| {
         let values = v.as_table().unwrap().keys().collect::<Vec<_>>();
         // Do another check to see if we have a special case of [key.value] inside of the file.
         if !values.windows(2).all(|w| w[0] <= w[1]) {
@@ -94,23 +71,18 @@ fn check_deps_by_key(manifest: &Manifest, key: &str) -> bool {
 }
 
 fn is_sorted(manifest: &Manifest) -> bool {
-    check_workspace(&manifest.value)
+    check_workspace(&manifest.table)
         && check_deps_by_key(manifest, "build-dependencies")
         && check_deps_by_key(manifest, "dependencies")
         && check_deps_by_key(manifest, "dev-dependencies")
 }
 
-fn main() {
-    let opt = Opt::from_args();
-    match parse_manifest(&opt) {
-        Ok(m) => {
-            if !is_sorted(&m) {
-                process::exit(1);
-            }
+fn main() -> anyhow::Result<()> {
+    let opt: Opt = argh::from_env();
+    parse_manifest(&opt).and_then(|m| {
+        if !is_sorted(&m) {
+            return Err(anyhow::anyhow!("manifest is not sorted"));
         }
-        Err(e) => {
-            eprintln!("error: {}", e);
-            process::exit(1);
-        }
-    }
+        Ok(())
+    })
 }
